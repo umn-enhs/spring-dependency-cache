b3d221872dcd5377efc5737fbf6f6793f184dba6
Natalya G. Portnov <ngp@umn.edu>
2012-05-16 11:28:45 -0500

    Initial commit

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,62 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenLocal()
+        //mavenCentral()
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+
+        // runtime 'mysql:mysql-connector-java:5.1.13'
+    }
+
+    plugins {
+		compile ":hibernate:$grailsVersion"
+		compile ":tomcat:$grailsVersion"
+
+		compile ":address-lookup-zpfour:0.1.2"
+		compile ":audit-logging:0.5.4"
+		compile ":barcode4j:0.2"
+		compile ":birt-report:2.6.1.0"
+		test ":code-coverage:1.2.5"
+		test ":codenarc:0.16.1"
+		compile ":csv:0.3"
+		compile ":joda-time:1.0"
+		compile ":jquery:1.7.1"
+		compile ":mail:1.0-SNAPSHOT"
+		compile ":pdf:0.6"
+		compile ":quartz:0.4.2"
+		compile ":springcache:1.3.1"
+		
+		compile ":webflow:$grailsVersion"
+
+		compile ":spring-security-core:1.2.7.3"
+		// If you're getting errors like the following, try running `grails compile` with the following version of 
+		// spring-security-ldap rather than the custom version
+		/* :: org.springframework.ldap#org.springframework.ldap;1.3.0.RELEASE: not found
+		   :: org.springframework.security#org.springframework.security.ldap;3.0.4.RELEASE: not found
+		   */
+		compile ":spring-security-ldap:1.0.5"
+		compile ":spring-security-shibboleth-native-sp:1.0.3"
+		provided ":spring-security-mock:1.0.1"
+	}
+}
